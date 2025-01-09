f240fa00626a58fe5f2f244fb6d989a23fbfa888
Archi-Savani <aaru.jbs07@gmail.com>
2025-01-09 15:28:03 +0530

    first push

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React from "react";
+import Header from "./components/global/Header/Header";
+import Home from "./pages/Home/Home";
+import { Route, Routes } from "react-router-dom";
+import AddProduct from "./pages/AddProduct";
+import { Box, CssBaseline, styled } from "@mui/material";
+import Sidebar from "./components/sidebar/Sidebar";
+import Login_form from "./pages/Login";
+
+const drawerWidth = 300;
+
+const App = () => {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [isClosing, setIsClosing] = React.useState(false);
+  const [open, setOpen] = React.useState(true);
+
+  const handleDrawerClose = () => {
+    setIsClosing(true);
+    setMobileOpen(false);
+  };
+  const handleDrawerToggle = () => {
+    if (!isClosing) {
+      setMobileOpen(!mobileOpen);
+    }
+  };
+  const handleDrawerTransitionEnd = () => {
+    setIsClosing(false);
+  };
+  const Main = styled("main", {
+    shouldForwardProp: (prop) => prop !== "open",
+  })(({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  }));
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+
+      <Header open={open} />
+
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        <Sidebar
+          open={open}
+          handleDrawerClose={handleDrawerClose}
+          window={window}
+          handleDrawerToggle={handleDrawerToggle}
+          mobileOpen={mobileOpen}
+          handleDrawerTransitionEnd={handleDrawerTransitionEnd}
+          setOpen={setOpen}
+        />
+      </Box>
+      <Main open={open}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add-product" element={<AddProduct />} />
+          <Route path="/login" element={<Login_form />} />
+        </Routes>
+      </Main>
+    </Box>
+  );
+};
+
+export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+
+const Home = () => {
+
+
+  return (
+    <Box mt={"50px"}>
+      Home
+    </Box>
+  );
+};
+
+export default Home;
